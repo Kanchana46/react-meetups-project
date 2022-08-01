5e8963c500d84b2704861928e1291e89dd8f86c2
Kanchana46 <kanchana.rdg@gmail.com>
2022-08-01 15:48:36 +0530

    integrating with firebase

--- src/pages/AllMeetup.js
+++ src/pages/AllMeetup.js
@@ -1,6 +1,7 @@
 import MeetupList from "../components/meeetups/MeetupList";
+import { useState, useEffect } from "react";
 
-const DUMMY_DATA = [
+/*const DUMMY_DATA = [
     {
         id: 'm1',
         title: 'This is a first meetup',
@@ -19,14 +20,61 @@ const DUMMY_DATA = [
         description:
             'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
     },
-];
+];*/
 
 function AllMeetups() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [loadedMeetups, setLoadMeetups] = useState([]);
+
+    useEffect(() => {
+        setIsLoading(true);
+        async function fecthMeetups() {
+            const response = await fetch('https://react-meetups-f787d-default-rtdb.firebaseio.com/meetups.json');
+            const meetupsList = [];
+            let data = await response.json();
+
+            for (const key in data) {
+                const meetup = {
+                    id: key,
+                    ...data[key]
+                }
+                meetupsList.push(meetup);
+                setIsLoading(false);
+                setLoadMeetups(meetupsList)
+            }
+        }
+        fecthMeetups()
+        /*fetch('https://react-meetups-f787d-default-rtdb.firebaseio.com/meetups.json').
+            then((response) => {
+                return response.json();
+            }).
+            then((data) => {
+                const meeetups = [];
+                console.log(data)
+                for (const key in data) {
+                    const meetup = {
+                        id: key,
+                        ...data[key]
+                    }
+                    meeetups.push(meetup)
+                }
+                setIsLoading(false);
+                setLoadMeetups(meeetups)
+            });*/
+    }, [])
+
+    if (isLoading) {
+        return (
+            <section>
+                <p>Loading...</p>
+            </section>
+        );
+    }
     return <section>
         <h1>
             All Meetups
         </h1>
-        <MeetupList meetups={DUMMY_DATA} />
+        <MeetupList meetups={loadedMeetups} />
     </section>
 }
 
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,9 +1,23 @@
+import { useNavigate } from 'react-router-dom'
 import NewMeetupForm from "../components/meeetups/NewMeetupForm";
 
 function NewMeetup() {
+    const navigate = useNavigate();
+    function addMeetupHandler(meetupData) {
+        fetch('https://react-meetups-f787d-default-rtdb.firebaseio.com/meetups.json', {
+            method: 'POST',
+            body: JSON.stringify(meetupData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(() => {
+            navigate('/', { replace: true })
+        });
+    }
+
     return <section>
         <h1>Add New Meetup</h1>
-        <NewMeetupForm />
+        <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
 }
 
